f1662b0ebec5b6d9bdb8fccbcb109c939cc5e5d7
akaya <darkness_ftv@mail.ru>
2022-12-28 01:39:34 +0800

    47

--- dz47/Program.cs
+++ dz47/Program.cs
@@ -6,40 +6,27 @@
 // 1,0 -3,3 8,0 -9,9
 // 8,0 7,8 -7,1 9,0
 
-// int[,] arr = new int[3, 4];
 
+double[,] array = new double[3, 4];
+// int linesCount = arr.GetLength(0);
+// int columnsCount = arr.GetLength(1);
 
-//  for (int i = 0; i < arr.GetLength(0); i++)
-//  {
-//      for (int j = 0; j < arr.GetLength(1); j++)
-//      {
-//          arr[i, j] = new Random().Next(-10, 10);
-//          Convert.ToDouble(arr[i,j]);
-//      }
-//  }
-//  for (int i = 0; i < arr.GetLength(0); i++)
-//  {
-//      for (int j = 0; j < arr.GetLength(1); j++)
-//      {
-//          Console.Write(arr[i, j] + ", ");
-//      }
-//      Console.WriteLine();
-//  }
-// // Console.Write = Convert.ToDouble(arr);
-// // Console.WriteLine();
-// //Convert.ToDouble("m, n");
+for (int i = 0; i < array.GetLength(0); i++)
 
-double[,] arr = new double[3, 4];
-
-for (int i = 0; i < 3; i++)
 {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        arr[i, j] = new Random().Next(-10, 10);
-        Console.Write("{0,6:F2}", arr[i, j]);
+        double number = new Random().Next(-10,10) + new Random().NextDouble();
+        number = Math.Round (number, 3);
+        array [i,j] = number;
+         Console.Write(array[i,j] + ", ");
+    
     }
     Console.WriteLine();
 }
+// arr[i, j] =Convert.ToSingle(linesCount);
+// arr[i, j] =Convert.ToSingle(columnsCount);
+
 // float result;
 // int m =3;
 // int n = 4;
